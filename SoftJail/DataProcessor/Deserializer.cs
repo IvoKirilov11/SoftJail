574b8b4702691df4d3352936153e147edee9fd28
IvoKirilov11 <mefistofel_1980@abv.bg>
2021-12-14 21:15:22 +0200

    Add project files.

---
+++ SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,155 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.Data.Models;
+    using SoftJail.Data.Models.Enums;
+    using SoftJail.DataProcessor.ImportDto;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    public class Deserializer
+    {
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            var sb = new StringBuilder();
+            
+            var departmentCells = JsonConvert.DeserializeObject<IEnumerable<DepartmentCellInputModel>>(jsonString);
+            var departmets = new List<Department>();
+            
+            foreach (var departmentCell in departmentCells)
+            {
+                if(!IsValid(departmentCell) || !departmentCell.Cells.All(IsValid) || !departmentCell.Cells.Any())
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                var department = new Department
+                {
+                    Name = departmentCell.Name,
+                    Cells = departmentCell.Cells.Select(x => new Cell
+                    {
+                        CellNumber = x.CellNumber,
+                        HasWindow = x.HasWindow
+                    })
+                    .ToList()
+                };
+                departmets.Add(department);
+
+                sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+            }
+            context.Departments.AddRange(departmets);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+
+            var sb = new StringBuilder();
+
+            var prisonerMails = JsonConvert.DeserializeObject<IEnumerable<PresenormailInputModel>>(jsonString);
+
+            var prisioners = new List<Prisoner>();
+
+            foreach (var prisonerMail in prisonerMails)
+            {
+                if (!IsValid(prisonerMail) || !prisonerMail.Mails.All(IsValid))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+                var isValidRelaseDate = DateTime.TryParseExact(prisonerMail.ReleaseDate, "dd/MM/yyyy", 
+                    CultureInfo.InvariantCulture,DateTimeStyles.None,out DateTime relaseDate);
+                var incarcerationDate = DateTime.ParseExact(prisonerMail.IncarcerationDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                var prisoner = new Prisoner
+                {
+                    FullName = prisonerMail.FullName,
+                    Nickname = prisonerMail.Nickname,
+                    Age = prisonerMail.Age,
+                    Bail = prisonerMail.Bail,
+                    CellId = prisonerMail.CellId,
+                    ReleaseDate = isValidRelaseDate ? (DateTime?)relaseDate : null,
+                    IncarcerationDate = incarcerationDate,
+                    Mails = prisonerMail.Mails.Select(x => new Mail
+                    {
+                        Description = x.Description,
+                        Sender = x.Sender,
+                        Address = x.Address
+
+                    })
+                    .ToList()
+                   
+                };
+                prisioners.Add(prisoner);
+
+                sb.AppendLine($"Imported {prisoner.FullName} {prisoner.Age} years old");
+            }
+            context.Prisoners.AddRange(prisioners);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            var sb = new StringBuilder();
+
+            var validOfficers = new List<Officer>();
+
+            var officerPrisoners = XmlConverter.Deserializer<OfficerPrisinorInputModel>(xmlString, "Officers");
+
+            foreach (var officerPrisinor in officerPrisoners)
+            {
+                if(!IsValid(officerPrisinor))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                var officer = new Officer
+                {
+                    FullName = officerPrisinor.Name,
+                    Salary = officerPrisinor.Money,
+                    DepartmentId = officerPrisinor.DepartmentId,
+                    Position = Enum.Parse<Position>(officerPrisinor.Position),
+                    Weapon = Enum.Parse<Weapon>(officerPrisinor.Weapon),
+                    OfficerPrisoners = officerPrisinor.Prisoners.Select(x => new OfficerPrisoner
+                    {
+                        PrisonerId = x.Id
+                    })
+                    .ToList()
+
+
+                };
+                validOfficers.Add(officer);
+
+                sb.AppendLine($"Imported {officer.FullName} ({officer.OfficerPrisoners.Count} prisoners)");
+            }
+            context.Officers.AddRange(validOfficers);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+        
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
---
+++ SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,69 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.Linq;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var result = context.Prisoners
+                .Where(x => ids.Contains(x.Id))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    CellNumber = x.Cell.CellNumber,
+                    Officers = x.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name
+                    })
+                    .OrderBy(x => x.OfficerName)
+                    .ToList(),
+                    TotalOfficerSalary = decimal.Parse(x.PrisonerOfficers
+                    .Sum(x => x.Officer.Salary).ToString("F2"))
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+            string finish = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+            return finish;
+                
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+
+            var name = prisonersNames.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            var result = context.Prisoners
+                .Where(x => name.Contains(x.FullName))
+                .Select(x => new PrisonerViewModel
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    IncarcerationDate = x.IncarcerationDate.ToString("yyyy-MM-dd"),
+                    EncryptedMessages = x.Mails.Select(e => new EncryptedMessagesViewModel
+                    {
+                        Description = string.Join("",e.Description.Reverse())
+                    })
+                    
+                    .ToArray()
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+            var xml = XmlConverter.Serialize(result, "Prisoners");
+
+            return xml;
+        }
+    }
+}
